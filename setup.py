22ca54564e344812eca8d44d4f080572ce930d0a
artemowkin <artyom.groshev2017@gmail.com>
2022-03-28 20:37:15 +0700

    Added readme, tox and setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="num_to_rus",
+    version="1.0.0",
+    author="Artemowkin",
+    author_email="artyom.groshev2017@gmail.com",
+    description="Небольшая библиотека для перевода чисел в слова русского языка",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/artemowkin/num_to_rus",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operation System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
